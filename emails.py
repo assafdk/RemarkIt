aa9913fac462c4a149741b659c02e34746458306
Yanir Calisar <yanir-pro@Yanirs-MacBook-Pro.local>
2016-01-11 00:01:12 +0200

    Use refresh token to generate new access token

--- emails.py
+++ emails.py
@@ -1,5 +1,6 @@
 __author__ = 'Assaf Dekel'
 
+import requests, json
 import ParseAPI as parse
 from simple_salesforce import Salesforce
 
@@ -24,9 +25,46 @@ parse.register2remoteDB()
 sfCred = parse.getSalesforceCredentials(user_account)
 salesforce_instance_url = sfCred['instance_url']
 salesforce_access_token = sfCred['access_token']
+salesforce_refresh_token = sfCred['refresh_token']
 
 sf = Salesforce(instance_url=salesforce_instance_url, session_id=salesforce_access_token)
-response = sf.query_all("SELECT Id, Email FROM Lead WHERE LastName = 'Abbott'")
+
+#import pytz
+#import datetime
+#end = datetime.datetime.now(pytz.UTC) # we need to use UTC as salesforce API requires this
+#sf.Contact.updated(end - datetime.timedelta(days=10), end)
+
+try:
+    response = sf.query_all("SELECT Id, Email FROM Lead WHERE LastName = 'Abbott'")
+except Exception, e:
+    if e.status == 401:
+        SALESFORCE_REFRESH_URL = "https://login.salesforce.com/services/oauth2/token"
+        SALESFORCE_CONSUMER_KEY = "3MVG98_Psg5cppyYH7Cios03svOf9hpZtPg.n0yTXRIKlnjy43.MNRgdLDbmBc3T5wK2IoYOaPLNlqBzNouzE"
+        SALESFORCE_CONSUMER_SECRET = "2132402812325087889"
+        url = SALESFORCE_REFRESH_URL
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + salesforce_access_token,
+            'X-PrettyPrint': '1'
+        }
+        params = {'grant_type': 'refresh_token',
+                  'client_id': SALESFORCE_CONSUMER_KEY,
+                  'client_secret': SALESFORCE_CONSUMER_SECRET,
+                  'refresh_token': salesforce_refresh_token }
+        result = requests.get(url, headers=headers, params=params)
+
+        newSfCred = json.loads(result.content)
+        sfCred['instance_url'] = newSfCred['instance_url']
+        sfCred['access_token'] = newSfCred['access_token']
+        sfCred['signature'] = newSfCred['signature']
+        sfCred['id'] = newSfCred['id']
+        sfCred['issued_at'] = newSfCred['issued_at']
+        parse.pushSalesforceCredentials(user_account,sfCred)
+
+        # try with new token
+        sf = Salesforce(instance_url=sfCred['instance_url'], session_id=sfCred['access_token'])
+        response = sf.query_all("SELECT Id, Email FROM Lead WHERE LastName = 'Abbott'")
+
 
 emailsList = []
 #response['records'][i]['Email']
@@ -34,24 +72,6 @@ for lead in response['records']:
     emailsList.append(lead['Email'])
 
 
-
-
-
-print emailsList
-sfCred = parse.getSalesforceCredentials()
-
-
-sfCred.instance_url
-sfCred.access_token
-
-#sf = Salesforce(instance_url=sfCred.instance_url, session_id=sfCred.access_token)
-#sf = Salesforce(instance_url='https://na1.salesforce.com', session_id='')
-
-
-#import pytz
-#import datetime
-#end = datetime.datetime.now(pytz.UTC) # we need to use UTC as salesforce API requires this
-#sf.Contact.updated(end - datetime.timedelta(days=10), end)
-
-
-#sf.query_all("SELECT Id, Email FROM Contact WHERE LastName = 'Jones'")
+"""
+From her: Google AdWords -> make emails list and upload
+"""
